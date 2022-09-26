8aaa62fae7adbb0b698cdc373023efa558fa2966
greenwhale00 <goreakay@gmail.com>
2022-09-26 10:19:45 +0900

    220926

---
+++ src/data/data.js
@@ -0,0 +1,22 @@
+const company = {
+    name:'나의 쇼핑몰',
+    address:'부산시 해운대구 바다',
+    tel:'051-209-0926',
+    fax:'051-022-0926',
+    ceo:'KHL'
+}
+
+
+const profile = {
+    slogan:'quality over quantity'
+}
+
+const content = [
+    {con:'슬로건 01', des:'내용 아주 긴 내용 01' },
+    {con:'슬로건 02', des:'내용 아주 긴 내용 02' },
+    {con:'슬로건 03', des:'내용 아주 긴 내용 03' },
+]
+
+
+
+export {company, profile, content}
